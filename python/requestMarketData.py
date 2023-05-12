c144775bbc38e51a4372c88550123b81adc16b8f
dmnth <kborodin1@gmail.com>
2023-05-12 15:21:18 +0300

    modified minApp to query data

---
+++ python/requestMarketData.py
@@ -0,0 +1,91 @@
+
+#! /usr/bin/env python3
+
+import logging
+import datetime
+from threading import Timer
+import ibapi
+import time
+from ibapi.wrapper import EWrapper
+from ibapi.client import EClient
+from ibapi.order import Order
+from ibapi.contract import Contract
+from ibapi.utils import decimalMaxString, floatMaxString, intMaxString
+
+
+class TestApp(EWrapper, EClient):
+
+    def __init__(self):
+        EWrapper.__init__(self)
+        EClient.__init__(self, self)
+
+    # WRAPPERS HERE
+
+    def error(self, reqId: int, errorCode: int, errorString: str,
+            advansedOrderreject):
+        super().error(reqId, errorCode, errorString, advansedOrderreject)
+        error_message = f'Error id: {reqId}, Error code: {errorCode}, ' \
+                        + f'Msg: {errorString}'
+
+    # Provides next valid identifier needed to place an order
+    # Indicates that the connection has been established and other messages can be sent from
+    # API to TWS
+    def nextValidId(self, orderId):
+        #super().nextValidId(orderId)
+        logging.debug(f"Next valid ID is set to {orderId}")
+        self.nextValidOrderId = orderId
+        self.start()
+        print(f"Next valid order ID: {orderId}")
+
+    def start(self):
+        self.reqContractDetails(self.nextValidOrderId, contract)
+        self.placeOrder(self.nextValidOrderId, contract, order)
+
+    def stop(self):
+        self.done = True
+        self.disconnect()
+
+def main():
+    try:
+        app = TestApp()
+        app.connect('192.168.1.167', 7496, clientId=0)
+        print(f'{app.serverVersion()} --- {app.twsConnectionTime().decode()}')
+        print(f'ibapi version: ', ibapi.__version__)
+#        Timer(15, app.stop).start()
+        app.run()
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    main()
+    def tickPrice(self, reqId: TickerId, tickType: TickType, price: float,
+                  attrib: TickAttrib):
+        super().tickPrice(reqId, tickType, price, attrib)
+        print("TickPrice. TickerId:", reqId, "tickType:", tickType,
+              "Price:", floatMaxString(price), "CanAutoExecute:", attrib.canAutoExecute,
+              "PastLimit:", attrib.pastLimit, end=' ')
+        if tickType == TickTypeEnum.BID or tickType == TickTypeEnum.ASK:
+            print("PreOpen:", attrib.preOpen)
+        else:
+            print()
+    # ! [tickprice]
+
+    @iswrapper
+    # ! [ticksize]
+    def tickSize(self, reqId: TickerId, tickType: TickType, size: Decimal):
+        super().tickSize(reqId, tickType, size)
+        print("TickSize. TickerId:", reqId, "TickType:", tickType, "Size: ", decimalMaxString(size))
+    # ! [ticksize]
+
+    @iswrapper
+    # ! [tickgeneric]
+    def tickGeneric(self, reqId: TickerId, tickType: TickType, value: float):
+        super().tickGeneric(reqId, tickType, value)
+        print("TickGeneric. TickerId:", reqId, "TickType:", tickType, "Value:", floatMaxString(value))
+    # ! [tickgeneric]
+
+    @iswrapper
+    # ! [tickstring]
+    def tickString(self, reqId: TickerId, tickType: TickType, value: str):
+        super().tickString(reqId, tickType, value)
+        print("TickString. TickerId:", reqId, "Type:", tickType, "Value:", value)
