66214f31f0b9862af9c50cbdaf57513b9233cd94
dmnth <kborodin1@gmail.com>
2023-09-19 18:38:37 +0300

    added some comments

--- python/cSharpMesCase/reqHistoricalData.py
+++ python/cSharpMesCase/reqHistoricalData.py
@@ -9,7 +9,7 @@ from ibapi.wrapper import EWrapper
 from ibapi.client import EClient
 from ibapi.order import Order
 from ibapi.contract import Contract
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Thread
 from contracts import CustomContracts 
 
@@ -33,24 +33,58 @@ class TestApp(EWrapper, EClient):
         super().historicalData(reqId, bar)
         self.historicalBars.append(bar)
 
+    def getNext40days(self, end):
+        if " US/Central" in end:
+            endDate = end.rstrip(" US/Central")
+
+        if " US/Eastern" in end:
+            endDate = end.rstrip(" US/Eastern")
+
+        date = datetime.strptime(endDate, "%Y%m%d %H:%M:%S")
+        newDate = date - timedelta(days=40) 
+        strNewDate = newDate.strftime("%Y%m%d %H:%M:%S US/Central")
+        return strNewDate 
+
+    def getTimeBetweenDateAndBar(self, end, barDate):
+        endDate = end.rstrip(' US/Eastern')
+        barDate = barDate.date.rstrip(' US/Eastern')
+        edate = datetime.strptime(endDate, "%Y%m%d %H:%M:%S")
+        bdate = datetime.strptime(barDate, "%Y%m%d %H:%M:%S")
+
+        difference = edate - bdate
+
+        return difference
+
     def historicalDataEnd(self, reqId, start, end):
         super().historicalDataEnd(reqId, start, end)
         #TODO: crop timezone id into separate string.
         #TODO: format bar date time to more human readable form
+        print("Starting from: ", start)
         timenow = datetime.now().strftime("%H:%M:%S")
+
         if len(self.historicalBars) != 0:
             print(f"{timenow} : Writing bars for {reqId}...")
-            with open(f'mesData/singleQuery360endDate.txt', 'a') as file:
-                file.write(f"\n{reqId} {start}-{end}\n")
+            with open(f'mesData/20230828/4days1hbars.txt', 'a') as file:
+                file.write(f"50*#\nSTART: {start} END: {end}\n50*#")
                 for bar in self.historicalBars:
-                    string = f"reqId: {reqId}, Date: {bar.date}, O: {bar.open}, H: {bar.high}, L: {bar.low}, C {bar.close}\n"
+                    timeDiff = self.getTimeBetweenDateAndBar(end, bar)
+                    string = f"{end.split()[0]}, TimePassed: {timeDiff}, reqId: {reqId}, Date: {bar.date}, O: {bar.open}, H: {bar.high}, L: {bar.low}, C {bar.close}\n"
                     file.write(string)
                 file.write('\n')
                 file.close()
                 print("Finished writing bars")
-        print("Historical data end for: ", reqId)
-#        self.disconnect()
-    
+
+        endDate = self.getNext40days(start)
+        endDate = '20230829 00:00:00'
+        contracts = CustomContracts()
+        contract = contracts.mecContractCuntFut()
+        print("Next date: ", endDate)
+#        self.nextValidOrderId += 1
+#        self.reqHistoricalData(self.nextValidOrderId, contract, endDate, 
+#                '40 D', '1 hour', 'TRADES', 0, 1, False, [])
+#        time.sleep(10)
+        self.disconnect()
+
     def contractDetails(self, reqId, contractDetails):
         super().contractDetails(reqId, contractDetails)
         print("contract details: ", reqId, contractDetails)
@@ -89,45 +123,23 @@ class TestApp(EWrapper, EClient):
     def start(self):
         # TWS will retunr data only for instrument's timezone or 
         # for time zone that is configured as local in TWS settings.
-#        timezone = "US/Eastern"
-        timezone = "US/Central"
+        timezone = "US/Eastern"
+#        timezone = "US/Central"
         querytime = f"20230518 15:00:00 {timezone}"
         contracts = CustomContracts()
         contract = contracts.mecContractCuntFut()
         print(self.clientId)
         self.reqContractDetails(self.nextValidOrderId, contract)
-        endDateTime= f"20230515 00:00:00 {timezone}"
+        endDateTime= f"20230919 00:00:00 {timezone}"
 #        endDate = f'20230418 16:30:00 {timezone}'
 #        endDate = f"20230211 16:30:00 {timezone}"
         endDate = ""
         self.reqHeadTimeStamp(self.nextValidOrderId, contract, "BID_ASK", True,
                 1)
 
-#        endDateTime = f"20230401 00:00:00 {timezone}"
-#        self.reqHistoricalData(self.nextValidOrderId, contract, endDateTime, 
-#                '80 D', '1 hour', 'TRADES', 0, 1, False, [])
-
-#        self.reqHistoricalData(self.nextValidOrderId, contract, endDateTime, 
-#                '80 D', '1 hour', 'TRADES', 0, 1, False, [])
-
-#        endDateTime = f"20230801 00:00:00 {timezone}"
-#        self.nextValidOrderId += 1 
-#        self.reqHistoricalData(self.nextValidOrderId, contract, "", 
-#                '260 D', '1 hour', 'TRADES', 0, 1, False, [])
-#
-#        self.nextValidOrderId += 1 
-#        self.reqHistoricalData(self.nextValidOrderId, contract, "", 
-#                '360 D', '1 hour', 'TRADES', 0, 1, False, [])
-
-#        self.nextValidOrderId += 1 
-#        endDateTime = f'20230918 20:20:00 {timezone}'
-#        self.reqHistoricalData(self.nextValidOrderId, contract, endDateTime, 
-#                '360 D', '1 hour', 'TRADES', 0, 1, False, [])
-
         self.nextValidOrderId += 1 
-#        endDateTime = f'20230918 20:20:00 {timezone}'
         self.reqHistoricalData(self.nextValidOrderId, contract, endDateTime, 
-                '360 D', '1 hour', 'TRADES', 0, 1, False, [])
+                '40 D', '1 hour', 'TRADES', 0, 1, False, [])
 
     def stop(self):
         self.done = True
@@ -155,7 +167,7 @@ def threadedExecution():
 def main():
     try:
         app = TestApp()
-        app.connect('192.168.0.211', 7496, clientId=0)
+        app.connect('192.168.43.222', 7496, clientId=0)
         print(f'{app.serverVersion()} --- {app.twsConnectionTime().decode()}')
         print(f'ibapi version: ', ibapi.__version__)
         app.run()
